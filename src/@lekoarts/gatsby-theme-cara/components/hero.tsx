be934c6d8e9ada583d180fb70f00c5275182b60b
Lukasz Winiarski <50294681+darthwiniar@users.noreply.github.com>
2020-10-14 15:10:14 +0200

    Update hero.tsx

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -7,6 +7,32 @@ import SVG from "./svg"
 import { UpDown, UpDownWide } from "../styles/animations"
 // @ts-ignore
 import Intro from "../sections/intro"
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+      <br />
+
+    </Box>
+  )
+}
 
 const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
   <div>
